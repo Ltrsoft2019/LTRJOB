40fbedbd9074a774ebf7c68f7cf2b1a4bff07a19
DREAMSTARTER <ganeshcollege10@gmail.com>
2023-10-20 17:38:37 +0530

    LoginFragment added

---
+++ app/src/main/java/com/ltrsoft/ltrjob/LoginFragment.java
@@ -0,0 +1,14 @@
+package com.ltrsoft.ltrjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_fragment);
+    }
+}
