853d071de8155e348103c80d8fdc0be0c41d98ab
Abdul Shaikh <shaikhharun54101@gmail.com>
2024-01-26 02:16:51 +0530

    commeted by harun

---
+++ app/src/main/java/com/ltrsoft/ltrjob/DEOCLasses/Login.java
@@ -0,0 +1,106 @@
+package com.ltrsoft.ltrjob.DEOCLasses;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.ltrsoft.ltrjob.Classes.Userclass;
+import com.ltrsoft.ltrjob.Fragment.NavigationDrawerFragment;
+import com.ltrsoft.ltrjob.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login {
+    private static final String BASE_URL = "https://job.ltr-soft.com/login1.php";
+
+    public  void loginuser(final Context context, FragmentManager fragmentManager, EditText loginEmail, EditText loginPassword, Button loginButton, ProgressBar bar){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, BASE_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("data = " + response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String msg = jsonObject.getString("Message");
+                    if (msg.equals("100")) {
+                        Toast.makeText(context.getApplicationContext(), "Login Successfully", Toast.LENGTH_SHORT).show();
+                        NavigationDrawerFragment navigationDrawerFragment = new NavigationDrawerFragment();
+                        fragmentManager.beginTransaction().replace(R.id.constraint, navigationDrawerFragment).commit();
+                        bar.setVisibility(View.GONE);
+                        loginButton.setVisibility(View.VISIBLE);
+                        SharedPreferences pref = context.getSharedPreferences("login", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = pref.edit();
+                        editor.putBoolean("flag", true)
+                                .apply();
+
+//                        // Sending Fragment
+                        JSONObject userData = jsonObject.getJSONObject("0");
+                        String user_id = userData.getString("user_id");
+
+
+                    } else if (msg.equals("200")) {
+                        Toast.makeText(context, "Invalid Password", Toast.LENGTH_LONG).show();
+                        bar.setVisibility(View.GONE);
+                        loginButton.setVisibility(View.VISIBLE);
+
+                    } else {
+                        Toast.makeText(context, "Student Not Found Please Create New Account", Toast.LENGTH_LONG).show();
+                        bar.setVisibility(View.GONE);
+                        loginButton.setVisibility(View.VISIBLE);
+
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "error = " + error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> map = new HashMap<>();
+                Userclass userclass=new Userclass();
+
+                map.put("email", userclass.setUser_email(loginEmail.getText().toString()));
+                map.put("password", userclass.setUser_password(loginPassword.getText().toString()));
+
+
+
+                return map;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(stringRequest);
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/ltrsoft/ltrjob/Fragment/LoginFragment.java
@@ -0,0 +1,97 @@
+package com.ltrsoft.ltrjob.Fragment;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ltrsoft.ltrjob.DEOCLasses.Login;
+import com.ltrsoft.ltrjob.R;
+
+
+public class LoginFragment extends Fragment {
+    public LoginFragment() {
+
+    }
+
+    private TextView forgot, createaccount, spinnertxt, document;
+    private Button loginButton;
+    private EditText loginEmail, loginPassword;
+    private ProgressBar bar;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.loginfragment, container, false);
+
+        loginEmail = v.findViewById(R.id.loginEmail);
+        loginPassword = v.findViewById(R.id.loginPassword);
+        loginButton = v.findViewById(R.id.loginButton);
+        bar = v.findViewById(R.id.progressBar);
+        forgot = v.findViewById(R.id.forgot);
+        createaccount = v.findViewById(R.id.create_new_account);
+        bar.setVisibility(View.GONE);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    Login login=new Login();
+                    login.loginuser(requireContext(), getParentFragmentManager(), loginEmail, loginPassword, loginButton, bar);
+                    loginButton.setVisibility(View.GONE);
+                    bar.setVisibility(View.VISIBLE);
+
+                } else {
+                    loginEmail.setError("Please Enter Email");
+                    loginPassword.setError("Please Enter Password");
+                }
+
+            }
+        });
+
+
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RegistrationFragment registrationFragment =new RegistrationFragment();
+                getFragmentManager().beginTransaction().replace(R.id.constraint, registrationFragment).addToBackStack(null).commit();
+
+
+            }
+
+        });
+
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Otpsend otpsend=new Otpsend();
+//                getFragmentManager().beginTransaction().replace(R.id.constraint, otpsend).addToBackStack(null).commit();
+
+            }
+        });
+
+
+
+        return v;
+    }
+
+}
